e70641af97a6597043a600ef95b48a6215d151e0
Roberto Bettazzoni <bettazzoni@gmail.com>
2024-03-30 12:03:45 +0100

    Factorize kata refactoring

--- factorize.py
+++ factorize.py
@@ -1,34 +1,31 @@
-from typing import List
-
-PRIMES = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+FIRST_100_PRIMES = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
 
 
-def list_of_primes(n):
-    global PRIMES
-    out_list: list[int] = list(PRIMES)
-    if n > 100:
-        for i in range(100, n + 1):
+class PrimeNumbers:
+    primes = list(FIRST_100_PRIMES)
+
+    @classmethod
+    def set_prime_list_for_testing(cls, prime_list):
+        cls.primes = prime_list
+
+    @classmethod
+    def get_prime_list_for_testing(cls):
+        return cls.primes
+
+    @classmethod
+    def get_prime_numbers_list_until(cls, n):
+        for i in range(cls.primes[-1] + 1, n + 1):
             for j in range(2, i):
                 if i % j == 0:
                     break
             else:
-                out_list.append(i)
-    return out_list
+                cls.primes.append(i)
+        return cls.primes
 
 
 def factorize(n):
-    if n == 1:
-        return [1]
-    if n <= 0:
-        return None
-    return _factorize_core(n, list_of_primes(n))
-
-
-def _factorize_core(number, prime_list):
-    if number in prime_list:
-        return [number]
-    else:
-        for div in prime_list:
-            if number % div == 0:
-                return [div] + _factorize_core(number // div, prime_list)
-        return None
+    prime_list = PrimeNumbers.get_prime_numbers_list_until(n)
+    for div in prime_list:
+        if n % div == 0:
+            return [div] + factorize(n // div)
+    return []
--- factorize_test.py
+++ factorize_test.py
@@ -1,45 +1,38 @@
-import unittest
-from factorize import factorize
+from factorize import factorize, PrimeNumbers, FIRST_100_PRIMES
+import pytest
+
+PrimeNumbers.set_prime_list_for_testing([2])
+
+@pytest.mark.parametrize("message, expected_starting_list, all_prime_to_this_number", (
+        ("Check PrimeNumbers(10)", (2, 3, 5, 7), 10),
+        ("Check PrimeNumbers(20)", (2, 3, 5, 7, 11, 13, 17), 20),
+        ("Check PrimeNumbers(100)", FIRST_100_PRIMES, 100),
+        ("Check PrimeNumbers(200)", FIRST_100_PRIMES, 200),
+))
+def test_prime_number(message, expected_starting_list, all_prime_to_this_number):
+    len_primes_before_test = len(PrimeNumbers.get_prime_list_for_testing())
+    first_expected_element_of_result= PrimeNumbers.get_prime_numbers_list_until(all_prime_to_this_number)[:len(expected_starting_list)]
+    assert tuple(expected_starting_list) == tuple(first_expected_element_of_result)
+    assert len(PrimeNumbers.get_prime_list_for_testing()) > len_primes_before_test
 
 
-class FactorizeTestCase(unittest.TestCase):
-
-    def test_1(self):
-        self.assertEqual([1], factorize(1))
-
-    def test_2(self):
-        self.assertEqual([2], factorize(2))
-
-    def test_3(self):
-        self.assertEqual([3], factorize(3))
-
-    def test_4(self):
-        self.assertEqual([2, 2], factorize(4))
-
-    def test_6(self):
-        self.assertEqual([2, 3], factorize(6))
-
-    def test_9(self):
-        self.assertEqual([3, 3], factorize(9))
-
-    def test_12(self):
-        self.assertEqual([2, 2, 3], factorize(12))
-
-    def test_15(self):
-        self.assertEqual([3, 5], factorize(15))
-
-    def test_19(self):
-        self.assertEqual([7, 11, 19], factorize(19 * 11 * 7))
-
-    def test_3x3x3(self):
-        self.assertEqual([3, 3, 3], factorize(27))
-
-    def test_big_number(self):
-        self.assertEqual([2, 19, 173], factorize(2 * 19 * 173))
-
-    def test_big_big_number(self):
-        self.assertEqual([31, 937], factorize(31 * 937))
+@pytest.mark.parametrize("message, expected_primes, number_to_factorize", (
+        ("test 2", [2], 2),
+        ("test 3", [3], 3),
+        ("test 4", (2, 2), 4),
+        ("test 6", (2, 3), 6),
+        ("test 9", (3, 3), 9),
+        ("test 12", (2, 2, 3), 12),
+        ("test 15", (3, 5), 15),
+        ("test 27", (3, 3, 3), 27),
+        ("test 1463", (7, 11, 19), 1463),
+        ("test 6574", (2, 19, 173), 6574),
+        ("test 9296", (2, 2, 2, 2, 7, 83), 9296),
+        ("test 45396", (2, 2, 3, 3, 97), 3492),
+))
+def test(message, expected_primes, number_to_factorize):
+    assert set(expected_primes) == set(factorize(number_to_factorize)), message
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
